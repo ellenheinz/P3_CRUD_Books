134811185a9550ee89ece356b1f04e70a2ebcca4
ellenheinz <ellenheinz@hotmail.com>
2022-09-12 23:52:05 -0300

    INSERT no Postgresql através de Python

--- Library.py
+++ Library.py
@@ -1,13 +1,26 @@
 import psycopg2 as pg
 
 con = pg.connect(host='localhost', database='Library', user='postgres', password='Ellen@2023')
-
 print("Conectado")
 
-select = con.cursor()
-select.execute("SELECT * FROM books")
-
-query = select.fetchall()
+sql = con.cursor()
+sql.execute("SELECT * FROM books")
+query = sql.fetchall()
 
 for res in query:
     print(res)
+
+sql_insert = "INSERT INTO books (bk_id, bk_name, bk_author)" \
+             "VALUES (8, 'The Lord of the Rings: The Fellowshipofthe Ring', 'J. R. R. Tolkien')," \
+                    "(9, 'The Lord of the Rings: The Two Towers', 'J. R. R. Tolkien')," \
+                    "(10, 'The Lord of the Rings: The Return of the King', 'J. R. R. Tolkien')"
+sql.execute(sql_insert)
+con.commit()
+
+sql.execute("SELECT * FROM books")
+query = sql.fetchall()
+
+for res in query:
+    print(res)
+
+con.close()
